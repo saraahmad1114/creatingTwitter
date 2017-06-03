ec04b33b4e1ed18ee8ceb0c2ab59670183319936
Sara <saraahmad1114@yahoo.com>
2017-06-03 15:11:27 -0400

    creating cells

--- TwitterCreation/TwitterCreation/HomeController.swift
+++ TwitterCreation/TwitterCreation/HomeController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class HomeController: UICollectionViewController{
+    
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .green 
+        collectionView?.backgroundColor = .green
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: self.cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:self.cellId, for: indexPath)
+        
+        return cell
     }
 
 
