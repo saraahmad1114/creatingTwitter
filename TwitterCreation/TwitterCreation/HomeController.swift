c6a78ade331b53a283a1c1dc413467a884f24d2f
Sara <saraahmad1114@yahoo.com>
2017-06-03 16:36:59 -0400

    configuring

--- TwitterCreation/TwitterCreation/HomeController.swift
+++ TwitterCreation/TwitterCreation/HomeController.swift
@@ -18,8 +18,8 @@ class wordCell: UICollectionViewCell{
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
+
 
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     
