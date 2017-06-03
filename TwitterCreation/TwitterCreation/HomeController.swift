1ae21288257bf44f873b2722fb53cdbc24312d94
Sara <saraahmad1114@yahoo.com>
2017-06-03 16:32:45 -0400

    configured the cells

--- TwitterCreation/TwitterCreation/HomeController.swift
+++ TwitterCreation/TwitterCreation/HomeController.swift
@@ -8,26 +8,43 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController{
+class wordCell: UICollectionViewCell{
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     
     let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .green
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: self.cellId)
+        collectionView?.backgroundColor = .white
+        collectionView?.register(wordCell.self, forCellWithReuseIdentifier: self.cellId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return 4
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier:self.cellId, for: indexPath)
         
-        cell.backgroundColor = .blue 
+        //cell.backgroundColor = .blue
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 
 
