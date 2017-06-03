a9aee0e644dc4cb3ab6aa191dde18bfea09e58a7
Sara <saraahmad1114@yahoo.com>
2017-06-03 16:44:49 -0400

    configuring contents of the cell

--- TwitterCreation/TwitterCreation/HomeController.swift
+++ TwitterCreation/TwitterCreation/HomeController.swift
@@ -12,12 +12,32 @@ class wordCell: UICollectionViewCell{
 
     override init(frame: CGRect){
         super.init(frame: frame)
-        backgroundColor = .yellow
+        setupViews()
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TEST TEST TEST"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    func setupViews() {
+        backgroundColor = .yellow
+        
+        addSubview(wordLabel)
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+ 
+    
     }
+    
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
 }
 
 
