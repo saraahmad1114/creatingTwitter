43fb22011e8cebf18e3a41c37cec6868e3465e62
Sara <saraahmad1114@yahoo.com>
2017-06-03 14:52:30 -0400

    added a collection view

--- TwitterCreation/TwitterCreation/AppDelegate.swift
+++ TwitterCreation/TwitterCreation/AppDelegate.swift
@@ -21,7 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window = UIWindow(frame:  UIScreen.main.bounds)
         self.window?.makeKeyAndVisible()
-        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        
+        let homeController = HomeController(collectionViewLayout: UICollectionViewFlowLayout())
+        
+        self.window?.rootViewController = UINavigationController(rootViewController: homeController)
     
         
         
---
+++ TwitterCreation/TwitterCreation/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  TwitterCreation
+//
+//  Created by Flatiron School on 6/3/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green 
+    }
+
+
+}
